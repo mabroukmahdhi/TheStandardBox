b54d6d2686a24d02d49c391d9619be5b1834c547
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-11-01 21:40:53 +0100

    BROKERS: Adding EntityBroker

---
+++ TheStandardBox.Core/Brokers/Entities/EntityBroker.cs
@@ -0,0 +1,24 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using TheStandardBox.Core.Models.Foundations.Standards;
+
+namespace TheStandardBox.Core.Brokers.Entities
+{
+    public class EntityBroker : IEntityBroker
+    {
+        public virtual string GetEntityName(Type type) => type.Name;
+
+        public virtual string GetEntityName<TModel>() => typeof(TModel).Name;
+
+        public virtual string GetRelativeApiUrl<TModel>()
+            where TModel : IStandardEntity
+        {
+            return $"api/{typeof(TModel).Name}s";
+        }
+    }
+}

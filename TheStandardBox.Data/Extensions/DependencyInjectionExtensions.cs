21de0bef79b1777c038db75110ae54925338c770
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-11-01 23:56:14 +0100

    DependencyInjection: AddTheStandardBoxData

---
+++ TheStandardBox.Data/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.Extensions.DependencyInjection;
+using TheStandardBox.Core.Extensions;
+using TheStandardBox.Core.Models.Foundations.Standards;
+using TheStandardBox.Data.Brokers.StandardStorages;
+using TheStandardBox.Data.Services.Foundations.Standards;
+using TheStandardBox.Data.Services.Standards;
+
+namespace TheStandardBox.Data.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static void AddTheStandardBoxData(this IServiceCollection services)
+        {
+            services.AddTheStandardBoxCore();
+        }
+
+        public static void AddStandardStorage<TEntity>(this IServiceCollection services)
+                    where TEntity : class, IStandardEntity
+        {
+            services.AddScoped<IStandardStorageBroker<TEntity>, StandardStorageBroker<TEntity>>();
+        }
+
+        public static void AddStandardFoundationService<TEntity>(this IServiceCollection services)
+                    where TEntity : class, IStandardEntity
+        {
+            services.AddScoped<IStandardService<TEntity>, StandardService<TEntity>>();
+        }
+    }
+}

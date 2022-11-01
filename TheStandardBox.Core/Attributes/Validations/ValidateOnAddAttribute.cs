32646b8cd5fe2091370c7a0a74c621adb51ec7c9
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-11-01 21:04:51 +0100

    DATA: Add Validation Attributes

---
+++ TheStandardBox.Core/Attributes/Validations/ValidateOnAddAttribute.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace TheStandardBox.Core.Attributes.Validations
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public class ValidateOnAddAttribute : Attribute
+    { }
+}

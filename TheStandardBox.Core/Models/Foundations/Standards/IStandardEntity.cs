12078f40dadbc6ba8c951d4ffd3526696f5e78d2
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-11-01 20:57:57 +0100

    DATA: Adding Standard Entity Interface

---
+++ TheStandardBox.Core/Models/Foundations/Standards/IStandardEntity.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace TheStandardBox.Core.Models.Foundations.Standards
+{
+    public interface IStandardEntity
+    {
+        Guid Id { get; set; }
+        DateTimeOffset CreatedDate { get; set; }
+        DateTimeOffset UpdatedDate { get; set; }
+    }
+}

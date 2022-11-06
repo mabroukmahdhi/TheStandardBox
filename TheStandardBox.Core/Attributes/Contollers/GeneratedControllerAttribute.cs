c5b2ec41d7b46b8f1f0fcd07a79c418c14f42165
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-11-06 15:37:40 +0100

    ATTRIBUTES: GeneratedController

---
+++ TheStandardBox.Core/Attributes/Contollers/GeneratedControllerAttribute.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace TheStandardBox.Core.Attributes.Contollers
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class GeneratedControllerAttribute : Attribute
+    {
+        public GeneratedControllerAttribute(string route)
+        {
+            Route = route;
+        }
+
+        public string Route { get; set; }
+    }
+}
---
+++ TheStandardBox.Data/Controllers/Providers/GenericTypeControllerFeatureProvider.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using TheStandardBox.Core.Attributes.Contollers;
+
+namespace TheStandardBox.Data.Controllers.Providers
+{
+    public class GenericTypeControllerFeatureProvider : IApplicationFeatureProvider<ControllerFeature>
+    {
+        public void PopulateFeature(IEnumerable<ApplicationPart> parts, ControllerFeature feature)
+        {
+            var currentAssembly = typeof(GenericTypeControllerFeatureProvider).Assembly;
+            var candidates = currentAssembly.GetExportedTypes().Where(x => x.GetCustomAttributes<GeneratedControllerAttribute>().Any());
+
+            foreach (var candidate in candidates)
+            {
+                feature.Controllers.Add(typeof(StandardController<>).MakeGenericType(candidate).GetTypeInfo());
+            }
+        }
+    }
+}
